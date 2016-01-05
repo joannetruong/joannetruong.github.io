0cfb70df020eda9a690bd6604805eb9859270d9d
joannetruong <truong.j@husky.neu.edu>
2016-01-05 00:00:19 -0500

    switching contact and index

--- js/contact.js
+++ js/contact.js
@@ -1,21 +1,5 @@
-$("#toggle").click(function(event) {
-  event.preventDefault();
-  if ($(this).hasClass("isDown")) {
-    $(".navbar-fixed-top").animate({
-      "margin-top": "-62px"
-    }, "fast");
-    $("#content").animate({
-      "margin-top": "5px"
-    }, "fast");
-    $(this).removeClass("isDown");
-  } else {
-    $(".navbar-fixed-top").animate({
-      "margin-top": "0px"
-    }, "fast");
-    $("#content").animate({
-      "margin-top": "80px"
-    }, "fast");
-    $(this).addClass("isDown");
-  }
-  return false;
+$(document).ready(function(){
+  $(".fadeMe").hide();
+  $(".fadeMe").fadeToggle("slow");
 });
+
--- js/landing.js
+++ js/landing.js
@@ -1,4 +1,21 @@
-$(document).ready(function(){
-  $(".fadeMe").hide();
-  $(".fadeMe").fadeToggle("slow");
+$("#toggle").click(function(event) {
+  event.preventDefault();
+  if ($(this).hasClass("isDown")) {
+    $(".navbar-fixed-top").animate({
+      "margin-top": "-62px"
+    }, "fast");
+    $("#content").animate({
+      "margin-top": "5px"
+    }, "fast");
+    $(this).removeClass("isDown");
+  } else {
+    $(".navbar-fixed-top").animate({
+      "margin-top": "0px"
+    }, "fast");
+    $("#content").animate({
+      "margin-top": "80px"
+    }, "fast");
+    $(this).addClass("isDown");
+  }
+  return false;
 });
